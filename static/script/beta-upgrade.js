1b3a4717ef9234c735933758521f1b39e36ce412
pdc <pdc@alleged.org.uk>
2011-05-13 08:35:44 +0100

    Added Émile to the Ender library so I can reinstate animation on beta upgrade page

--- static/script/beta-upgrade.js
+++ static/script/beta-upgrade.js
@@ -1,6 +1,9 @@
 
 $.domReady(function () {
+  var prevID = null;
+  var heights = {};
   function up() {
+    var animationMilliseconds = !prevID ? 0 : 333;
     var recipeID = -1;
     $('select[name="recipe"] option').each(function (el) {
       if (el.selected) {
@@ -8,10 +11,20 @@ $.domReady(function () {
       }
     });
     var sectionID = 'recipe-' + recipeID;
-    $('#more section').each(function (el) {
-        el.className = (el.id == sectionID ? 'shown' : 'hidden');
-    });
+    if (sectionID != prevID) {
+      $('#more section').each(function (el) {
+        if (!prevID) {
+          heights[el.id] = $(el).css('height')
+        }
+        if (el.id == sectionID) {
+          $(el).animate({height: heights[el.id], duration: animationMilliseconds});
+        } else if (!prevID || el.id == prevID) {
+          $(el).animate({height: 0, overflow: 'hidden', duration: animationMilliseconds})
+        }
+      });
+      prevID = sectionID;
+    }
   }
-  up();
+  up(0);
   $('select[name="recipe"]').bind('change', up);
 });
